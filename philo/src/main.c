f6b610067d9b3639144adbcb28f094b099d1cb25
Théo Bouverat <theo@MacBook-Pro-de-Theo.local>
2023-10-11 13:58:48 +0200

    update

---
+++ philo/src/main.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbouvera <tbouvera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 11:52:11 by tbouvera          #+#    #+#             */
+/*   Updated: 2023/07/21 14:25:44 by tbouvera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+
+
+int	main(int argc, char **argv)
+{
+	int				i;
+	t_table			*table;
+	pthread_t		*threads;
+	pthread_t		*monitor;
+	t_philo_context	*philo_data;
+
+	i = 0;
+	table = init_table(argc, argv);
+	threads = malloc(sizeof(pthread_t) * table->n_p);
+	monitor = malloc(sizeof(pthread_t));
+	philo_data = malloc(sizeof(t_philo_context) * table->n_p);
+	if (!threads || !philo_data || !monitor)
+		exit(1);
+	pthread_create(monitor, NULL, &monitor_routine, table);
+	while (i < table->n_p)
+	{
+		philo_data[i].philosopher = &table->philosophers[i];
+		philo_data[i].table = table;
+		pthread_create(&threads[i], NULL, &philo_routine, &philo_data[i]);
+		//pthread_create(&monitor[i], NULL, &monitor_routine, &philo_data[i]);
+		i ++;
+	}
+	i = 0;
+	pthread_join(*monitor, NULL);
+	while (i < table->n_p)
+	{
+		pthread_join(threads[i], NULL);
+		//pthread_join(monitor[i], NULL);
+		i ++;
+	}
+	exit(0);
+}
---
+++ philo/src/philo_behaviour.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_behaviour.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbouvera <tbouvera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 11:52:11 by tbouvera          #+#    #+#             */
+/*   Updated: 2023/07/21 14:25:44 by tbouvera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	eat(t_philosopher *philosopher, t_table *table)
+{
+		pthread_mutex_lock(&philosopher->left_fork->mutex);
+		if (table->stop != 1) {
+			log_action(philosopher, "has taken a fork", table);
+		}
+		pthread_mutex_lock(&philosopher->right_fork->mutex);
+		if (table->stop != 1) {
+			log_action(philosopher, "has taken a fork", table);
+			log_action(philosopher, "is eating", table);
+		}
+		usleep(table->time_to_eat * 1000);
+		philosopher->last_meal_time = get_time();
+		pthread_mutex_unlock(&philosopher->left_fork->mutex);
+		pthread_mutex_unlock(&philosopher->right_fork->mutex);
+		philosopher->meals_eaten++;
+}
+
+void	sleep_and_think(t_philosopher *philosopher, t_table *table)
+{
+	if (table->stop != 1) {
+		log_action(philosopher, "is sleeping", table);
+	}
+	usleep(table->time_to_sleep * 1000);
+	if (table->stop != 1) {
+		log_action(philosopher, "is thinking", table);
+	}
+}
+
+void	monitor_loop(t_table *table)
+{
+	int i;
+	int x;
+
+	i = 0;
+	x = 0;
+	while (1)
+	{
+		usleep(100);
+		while (x < table->n_p)
+		{
+			t_philosopher philo = table->philosophers[x];
+
+			if (get_time() - philo.last_meal_time > table->time_to_die)
+			{
+				if (table->stop == 0) {
+					log_action(&philo, "died", table);
+					while (i < table->n_p) {
+						table->philosophers[i].died = 1;
+						i ++;
+					}
+					table->stop = 1;
+				}
+				exit(0);
+			}
+			if (table->max_eat != -1 && philo.meals_eaten >= table->max_eat)
+			{
+				pthread_mutex_lock(&table->end_mutex);
+				table->philosophers_done++;
+				if (table->philosophers_done >= table->n_p)
+					exit(0);
+				pthread_mutex_unlock(&table->end_mutex);
+			}
+			x ++;
+		}
+		x = 0;
+	}
+}
+
+void	*monitor_routine(void *arg)
+{
+	t_table			*table;
+
+	table = (t_table *)arg;
+	monitor_loop(table);
+	return (NULL);
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philo_context	*context;
+	t_philosopher	*philosopher;
+	t_table			*table;
+
+	context = (t_philo_context *)arg;
+	philosopher = context->philosopher;
+	table = context->table;
+	philosopher->last_meal_time = get_time();
+	while (philosopher->died != 1 && (philosopher->meals_eaten < table->max_eat || table->max_eat == -1))
+	{
+		eat(philosopher, table);
+		sleep_and_think(philosopher, table);
+	}
+	return (NULL);
+}
