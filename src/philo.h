ca38eb7ba1b2ede6d1547e7f19797ce59c26ba8c
Théo Bouverat <theo@MacBook-Pro-de-Theo.local>
2023-10-13 17:55:50 +0200

    norm

--- src/philo.h
+++ src/philo.h
@@ -19,15 +19,6 @@
 # include <unistd.h>
 # include <time.h>
 
-# define RESET_COLOR "\033[0m"
-# define RED_COLOR "\033[31m"
-# define GREEN_COLOR "\033[32m"
-# define YELLOW_COLOR "\033[33m"
-# define BLUE_COLOR "\033[34m"
-# define MAGENTA_COLOR "\033[35m"
-# define CYAN_COLOR "\033[36m"
-# define WHITE_COLOR "\033[37m"
-
 typedef struct s_fork {
 	pthread_mutex_t	mutex;
 }		t_fork;
