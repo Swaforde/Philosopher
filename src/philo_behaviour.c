17507b55d36ca4bf9daab7f504178b6cca9305ae
Théo Bouverat <theo@MacBook-Pro-de-Theo.local>
2023-10-15 13:31:58 +0200

    update

--- src/philo_behaviour.c
+++ src/philo_behaviour.c
@@ -17,38 +17,30 @@ int	eat(t_philosopher *philosopher, t_table *table)
 	if (philosopher->id % 2 != 0)
 	{
 		pthread_mutex_lock(&philosopher->left_fork->mutex);
-		//if (table->stop != 1)
-			log_action(philosopher, "has taken a fork", table);
+		log_action(philosopher, "has taken a fork", table);
 		if (table->n_p == 1) {
 			custom_sleep(table->time_to_die+1, philosopher, table);
 			pthread_mutex_unlock(&philosopher->left_fork->mutex);
 			return 1;
 		}
 		pthread_mutex_lock(&philosopher->right_fork->mutex);
-		//if (table->stop != 1)
-			log_action(philosopher, "has taken a fork", table);
+		log_action(philosopher, "has taken a fork", table);
 	}
 	else
 	{
 		pthread_mutex_lock(&philosopher->right_fork->mutex);
-		//if (table->stop != 1)
-			log_action(philosopher, "has taken a fork", table);
+		log_action(philosopher, "has taken a fork", table);
 		pthread_mutex_lock(&philosopher->left_fork->mutex);
-		//if (table->stop != 1)
-			log_action(philosopher, "has taken a fork", table);
+		log_action(philosopher, "has taken a fork", table);
 	}
-	//if (table->stop != 1)
-	//{
-		log_action(philosopher, "is eating", table);
-		philosopher->last_meal_time = get_time();
-		if(custom_sleep(table->time_to_eat, philosopher, table))
-			return 1;
-	//}
+	log_action(philosopher, "is eating", table);
+	philosopher->last_meal_time = get_time();
+	if(custom_sleep(table->time_to_eat, philosopher, table))
+		return 1;
 	pthread_mutex_unlock(&philosopher->left_fork->mutex);
 	pthread_mutex_unlock(&philosopher->right_fork->mutex);
 	philosopher->meals_eaten++;
 	if (philosopher->meals_eaten >= table->max_eat && table->max_eat != -1) {
-		//table->stop = 1;
 		table->philosophers_done++;
 		return (1);
 	}
@@ -57,12 +49,10 @@ int	eat(t_philosopher *philosopher, t_table *table)
 
 int	sleep_and_think(t_philosopher *philosopher, t_table *table)
 {
-	//if (table->stop != 1)
-		log_action(philosopher, "is sleeping", table);
+	log_action(philosopher, "is sleeping", table);
 	if(custom_sleep(table->time_to_sleep, philosopher, table))
 		return 1;
-	//if (table->stop != 1)
-		log_action(philosopher, "is thinking", table);
+	log_action(philosopher, "is thinking", table);
 	return 0;
 }
 
@@ -97,25 +87,15 @@ void	monitor_loop(t_table *table)
 	usleep(100);
 	while (1)
 	{
-		//i = 0;
-		//while (i < table->n_p)
-		//{
-			t_philosopher *philo = &table->philosophers[i++];
-			if (table->stop == 1)
-			{
-				die_log(table, philo);
-				table->stop = 1;
-				return ;
-			}
-			if (table->philosophers_done >= table->n_p)
-					return ;
-			/*if (table->max_eat != -1 && philo->meals_eaten >= table->max_eat)
-			{
-				pthread_mutex_lock(&table->end_mutex);
-				table->philosophers_done++;
-				pthread_mutex_unlock(&table->end_mutex);
-			}*/
-		//}
+		t_philosopher *philo = &table->philosophers[i++];
+		if (table->stop == 1)
+		{
+			die_log(table, philo);
+			table->stop = 1;
+			return ;
+		}
+		if (table->philosophers_done >= table->n_p)
+			return ;
 	}
 }
 
